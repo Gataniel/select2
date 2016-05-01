536b43edeaacd454d51e941aac7bf7a623103bd9
Azat Gataullin <gataniel@gmail.com>
2016-05-02 00:55:13 +0300

    Fix scroll after removing elemets

--- dist/js/select2.js
+++ dist/js/select2.js
@@ -4186,7 +4186,10 @@ S2.define('select2/dropdown/attachBody',[
 
     $watchers.on(scrollEvent, function (ev) {
       var position = $(this).data('select2-scroll-position');
-      $(this).scrollTop(position.y);
+      if($(this).find('.select2-container--open').size()>0)
+      {
+          $(this).scrollTop(position.y);
+      }
     });
 
     $(window).on(scrollEvent + ' ' + resizeEvent + ' ' + orientationEvent,
